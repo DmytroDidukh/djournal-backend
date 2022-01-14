373b4259440417c844db89f4923f2d3a562f212b
Dmytro Didukh <jeriko.outback@gmail.com>
2022-01-14 22:41:54 +0200

    feat/comments: comments module

---
+++ src/comment/comment.service.ts
@@ -0,0 +1,57 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { UpdateCommentDto } from './dto/update-comment.dto';
+import { CommentEntity } from './entities/comment.entity';
+
+@Injectable()
+export class CommentService {
+  constructor(
+    @InjectRepository(CommentEntity)
+    private commentRepository: Repository<CommentEntity>,
+  ) {}
+
+  create(dto: CreateCommentDto) {
+    return this.commentRepository.save({
+      text: dto.text,
+      post: { id: dto.postId },
+      user: { id: 2 },
+    });
+  }
+
+  findAll() {
+    return this.commentRepository.find();
+  }
+
+  async findOneById(id: number) {
+    const result = await this.commentRepository.findOne(+id);
+
+    if (!result) {
+      throw new NotFoundException();
+    }
+
+    return result;
+  }
+
+  async update(id: number, dto: UpdateCommentDto) {
+    const result = await this.findOneById(id);
+
+    if (!result) {
+      throw new NotFoundException();
+    }
+
+    return this.commentRepository.update(id, dto);
+  }
+
+  async remove(id: number) {
+    const result = await this.findOneById(id);
+
+    if (!result) {
+      throw new NotFoundException();
+    }
+
+    return this.commentRepository.delete(id);
+  }
+}
